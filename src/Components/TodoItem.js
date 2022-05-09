f5f3a7f23ea4d47847466fd663b01e3ad7e43a9b
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-05-10 00:02:12 +0530

    added checked and delete functionality

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,15 +1,38 @@
 import React from "react";
 import { FaTrash, FaCheck } from "react-icons/fa"
 
+
+
+
 const TodoItem = ({ todoList, setTodoList }) => {
+
+    const completeTaskHandler = (todo) => {
+        setTodoList(
+            todoList.map((element) => {
+                if(element.id === todo.id){
+                    return {...element, completed: !element.completed}
+                }
+                return element
+            })
+        )
+    }
+
+    const deleteTaskHandler = (todo) => {
+        setTodoList(todoList.filter(
+            (element) => element.id !== todo.id
+        ))
+    }
+
     return (
         <div className="todo-list">
             {todoList.map((todo) => (
                 <div className="todo-item">
+                    <div style={{display: "inline"}}className={todo.completed ? "checked" : ""}>
                     {todo.task}
+                    </div>
                     <div className="todo-icon">
-                    <FaCheck className="todo-icon" size="1.5rem" />
-                    <FaTrash className="todo-icon" size="1.5rem" />
+                    <FaCheck onClick={() => completeTaskHandler(todo)} className="todo-icon" size="1.5rem" />
+                    <FaTrash onClick={() => deleteTaskHandler(todo)} className="todo-icon" size="1.5rem" />
                 </div>
                 </div>
             )
